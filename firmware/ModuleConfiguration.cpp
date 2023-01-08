c7128f2e6ed95976af0d9a0b86ada59a33c3fb37
Paul Reeve <preeve@pdjr.eu>
2023-01-08 11:48:12 +0100

    Stuff

--- firmware/ModuleConfiguration.cpp
+++ firmware/ModuleConfiguration.cpp
@@ -3,9 +3,11 @@
 #include <EEPROM.h>
 #include "ModuleConfiguration.h"
 
-ModuleConfiguration::ModuleConfiguration(unsigned int size, unsigned int eepromAddress) {
+ModuleConfiguration::ModuleConfiguration(unsigned int size, unsigned int eepromAddress, void (*callback)(unsigned int, unsigned char)) {
   this->size = size;
   this->eepromAddress = eepromAddress;
+  this->callback = callback;
+
   this->configuration = new unsigned char [this->size];
   for (unsigned int i = 0; i < this->size; i++) this->configuration[i] = 0xff;
 }
@@ -13,6 +15,7 @@ ModuleConfiguration::ModuleConfiguration(unsigned int size, unsigned int eepromA
 void ModuleConfiguration::setByte(unsigned int index, unsigned char value) {
   if (index < this->size) {
     this->configuration[index] = value;
+    if (this->callback) this->callback(index, value);
   }
 }
 
@@ -27,6 +30,39 @@ bool ModuleConfiguration::inUse() {
   bool retval = false;
 
   for (unsigned int i = 0; i < this->size; i++) if (this->configuration[i] != 0xff) retval = true;
+  return(retval);
+}
+
+bool ModuleConfiguration::interact(int value, bool longPress) {
+  static long resetDeadline = 0UL;
+  static int address = -1;
+  long now = millis();
+  bool retval;
+   
+  if (value == 0xffff) { // Perhaps cancel a timed-out protocol.
+    if ((resetDeadline != 0UL) && (now > resetDeadline)) {
+      address = -1;
+      resetDeadline = 0UL;
+      retval = false;
+    }
+  } else {
+    switch (longPress) {
+      case true:
+        if ((value >= 0) && (value < EEPROM.length())) {
+          address = value;
+          retval = true;
+          resetDeadline = (now + CONFIGURATION_INACTIVITY_TIMEOUT);
+        }
+        break;
+      case false:
+        if (address != -1) {
+          this->setByte(address, (unsigned char) value);
+          address = -1;
+          retval = false;
+        }
+        break;
+    }
+  }
   return(retval);
 }
 
--- firmware/ModuleConfiguration.h
+++ firmware/ModuleConfiguration.h
@@ -4,9 +4,11 @@
 class ModuleConfiguration {
 
   public:
-    ModuleConfiguration(unsigned int size, unsigned int eepromAddress = 0);
+    ModuleConfiguration(unsigned int size, unsigned int eepromAddress = 0, void (*callback)(unsigned int, unsigned char) = 0);
     void setByte(unsigned int index, unsigned char value);
     unsigned char getByte(unsigned int index);
+
+    bool ModuleConfiguration::interact(int value, bool longPress);
 
     bool inUse();
 
@@ -16,6 +18,8 @@ class ModuleConfiguration {
   private:
     unsigned int size;
     unsigned int eepromAddress;
+    void (*callback)(unsigned int address, unsigned char value);
+  
     unsigned char *configuration;
     
 };
